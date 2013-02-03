d91d18c47eb3202e8d61fe77d9f00fde0a672244
busticated <itsbusticated@gmail.com>
2013-02-03 14:03:06 -0800

    setting up gh-pages

---
+++ js/main.js
@@ -0,0 +1,12 @@
+﻿require.config({
+    paths : {
+        jquery : 'lib/jquery'
+    },
+    waitSeconds: 180
+});
+require([ 'jquery', 'mods/checksnap' ], function( $, CheckSnap ){
+    'use strict';
+
+    var chksnp = new CheckSnap();
+    chksnp.setup();
+});
